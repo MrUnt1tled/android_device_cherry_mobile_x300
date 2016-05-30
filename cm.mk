737ce9aa989a3beb5ee5da135e0c4d703efbc5ef
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-05-30 13:43:35 +0800

    Update to x300

--- cm.mk
+++ cm.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := m2
+PRODUCT_RELEASE_NAME := x300
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -8,16 +8,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/gionee/m2/device_m2.mk)
+$(call inherit-product, device/cherrymobile/x300/device_x300.mk)
 
 # Inherit Proprietary Stuff
-$(call inherit-product, vendor/gionee/m2/m2-vendor.mk)
+$(call inherit-product, vendor/cherrymobile/x300/x300-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m2
-PRODUCT_NAME := cm_m2
-PRODUCT_BRAND := gionee
-PRODUCT_MODEL := m2
-PRODUCT_MANUFACTURER := gionee 
+PRODUCT_DEVICE := x300
+PRODUCT_NAME := cm_x300
+PRODUCT_BRAND := cherrymobile
+PRODUCT_MODEL := FlareS3_Power
+PRODUCT_MANUFACTURER := cherrymobile 
 
 
