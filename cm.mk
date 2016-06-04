285b90deeb2b1a08d23c710f648cd5ef42eee899
John Reinel Caluag <johnrenielcaluag1009@gmail.com>
2016-06-04 18:21:20 +0800

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,17 +1,14 @@
 # Release name
 PRODUCT_RELEASE_NAME := x300
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit Complete Device Configuration
+$(call inherit-product, device/cherrymobile/x300/full_x300.mk)
+
+# Inherit Suitabe GPS Code
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/cherrymobile/x300/device_x300.mk)
-
-# Inherit Proprietary Stuff
-$(call inherit-product, vendor/cherrymobile/x300/x300-vendor.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x300
